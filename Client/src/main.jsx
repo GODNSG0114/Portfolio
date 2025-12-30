cbaeff6b59c72423ab0f93454770e63c0131f8dc
Nikhil God <nikhil0114@gmail.com>
2025-12-30 19:56:54 +0530

    Fix import case sensitivity for Daily_que_Context component

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Daily_que_Context from './Context/daily_que_Context.jsx'
+import Daily_que_Context from './Context/Daily_que_Context.jsx'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
